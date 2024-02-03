718bddae65d44fbb9cfaad724a65f7c5b2fed677
moham <mohamedhesen0@gmail.com>
2024-02-03 02:29:06 +0200

    Implement Animated Splash Screen

--- app/src/main/java/com/example/alquran/ui/SplashFragment.kt
+++ app/src/main/java/com/example/alquran/ui/SplashFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.alquran.R
+import com.example.alquran.databinding.FragmentSplashBinding
+import com.example.alquran.databinding.SurahDetailsItemBinding
 
 class SplashFragment : Fragment() {
 
-
+    lateinit var splashBinding: FragmentSplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +21,34 @@ class SplashFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        splashBinding = FragmentSplashBinding.inflate(layoutInflater,container,false)
+
+
+        return splashBinding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+        splashBinding.img.apply {
+            alpha = 0f
+            scaleX = 0f
+            scaleY =0f
+
+        }
+
+        splashBinding.img.animate().setDuration(6000).scaleX(1.2f)
+            .scaleY(1.2f).alpha(1f).withEndAction{
+                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+
+            }.start()
+
+    }
+
+
 
 
 }
